df7399138aa14b8b186964b52934dc25db06ce02
Tomas Vazquez <vazquezt2018@gmail.com>
2024-08-10 19:33:45 -0300

    fix(config): add types in functions

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=["start", "help"])
-def send_message(message):
+def send_message(message) -> None:
     bot.reply_to(
         message,
         "Hola! Soy un bot que te permite obtener información sobre el dólar y el bitcoin en Argentina. Para obtener información sobre el dólar, escribe /dolar. Para obtener información sobre el bitcoin, escribe /bitcoin.",
@@ -20,14 +20,14 @@ def send_message(message):
 
 
 @bot.message_handler(commands=["dolar"])
-def dolar_price(message):
+def dolar_price(message) -> None:
     dolar_scraper = DolarScrapper(dolar_url)
     compra, venta = dolar_scraper.scrape_dolar_values()
     bot.reply_to(message, dolar_scraper.send_dollar_price(compra, venta))
 
 
 @bot.message_handler(commands=["bitcoin"])
-def bitcoin_price(message):
+def bitcoin_price(message) -> None:
     bitcoin_scraper = BitcoinScrapper(bitcoin_url)
     compra, venta = bitcoin_scraper.scrape_bitcoin_values()
     bot.reply_to(message, bitcoin_scraper.send_bitcoin_price(compra, venta))
